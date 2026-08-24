b14a4fd8200b37d55bb088503f2eb0e1ce4f0091
João Sousa <joaosantosesousa@gmail.com>
2026-08-24 22:13:30 +0100

    Adicionar criacao de categorias com Entity Framework

---
+++ PortalOcorrenciasIPT/Pages/CriarCategoria.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PortalOcorrenciasIPT.Data;
+using PortalOcorrenciasIPT.Models;
+
+namespace PortalOcorrenciasIPT.Pages;
+
+public class CriarCategoriaModel : PageModel
+{
+    private readonly ApplicationDbContext _context;
+
+    public CriarCategoriaModel(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [BindProperty]
+    public Categoria Categoria { get; set; } = new();
+
+    public void OnGet()
+    {
+    }
+
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        _context.Categorias.Add(Categoria);
+        _context.SaveChanges();
+
+        TempData["MensagemSucesso"] = "Categoria criada com sucesso.";
+
+        return RedirectToPage("/Categorias");
+    }
+}
