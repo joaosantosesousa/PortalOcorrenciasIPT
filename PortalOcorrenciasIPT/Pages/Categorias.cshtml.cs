8b52401f414f13ca629684cd80756720da80d31f
João Sousa <joaosantosesousa@gmail.com>
2026-08-24 21:52:34 +0100

    Listar categorias a partir da base de dados

--- PortalOcorrenciasIPT/Pages/Categorias.cshtml.cs
+++ PortalOcorrenciasIPT/Pages/Categorias.cshtml.cs
@@ -1,12 +1,22 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PortalOcorrenciasIPT.Data;
+using PortalOcorrenciasIPT.Models;
 
-namespace PortalOcorrenciasIPT.Pages
+namespace PortalOcorrenciasIPT.Pages;
+
+public class CategoriasModel : PageModel
 {
-    public class CategoriasModel : PageModel
+    private readonly ApplicationDbContext _context;
+
+    public CategoriasModel(ApplicationDbContext context)
     {
-        public void OnGet()
-        {
-        }
+        _context = context;
+    }
+
+    public List<Categoria> Categorias { get; set; } = new();
+
+    public void OnGet()
+    {
+        Categorias = _context.Categorias.ToList();
     }
 }
