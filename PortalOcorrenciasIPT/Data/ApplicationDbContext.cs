8e9e4e0238bbe85aaec62959782aec64c40d0122
João Sousa <joaosantosesousa@gmail.com>
2026-08-24 18:58:41 +0100

    Configurar Entity Framework Core para categorias

---
+++ PortalOcorrenciasIPT/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortalOcorrenciasIPT.Models;
+
+namespace PortalOcorrenciasIPT.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Categoria> Categorias { get; set; }
+}
---
+++ PortalOcorrenciasIPT/Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortalOcorrenciasIPT.Models;
+
+public class Categoria
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O nome da categoria é obrigatório.")]
+    [StringLength(80, ErrorMessage = "O nome da categoria não pode exceder 80 caracteres.")]
+    public string Nome { get; set; } = "";
+
+    [StringLength(250, ErrorMessage = "A descrição não pode exceder 250 caracteres.")]
+    public string? Descricao { get; set; }
+
+    public bool Ativa { get; set; } = true;
+}
