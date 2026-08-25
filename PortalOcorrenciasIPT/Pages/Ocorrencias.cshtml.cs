5c3f98d3d69228fc239e6de64f491fcedb80462c
João Sousa <joaosantosesousa@gmail.com>
2026-08-25 20:28:19 +0100

    Listar ocorrencias a partir da base de dados

--- PortalOcorrenciasIPT/Pages/Ocorrencias.cshtml.cs
+++ PortalOcorrenciasIPT/Pages/Ocorrencias.cshtml.cs
@@ -1,12 +1,26 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PortalOcorrenciasIPT.Data;
+using PortalOcorrenciasIPT.Models;
 
-namespace PortalOcorrenciasIPT.Pages
+namespace PortalOcorrenciasIPT.Pages;
+
+public class OcorrenciasModel : PageModel
 {
-    public class OcorrenciasModel : PageModel
+    private readonly ApplicationDbContext _context;
+
+    public OcorrenciasModel(ApplicationDbContext context)
     {
-        public void OnGet()
-        {
-        }
+        _context = context;
+    }
+
+    public List<Ocorrencia> Ocorrencias { get; set; } = new();
+
+    public void OnGet()
+    {
+        Ocorrencias = _context.Ocorrencias
+            .Include(ocorrencia => ocorrencia.Categoria)
+            .OrderByDescending(ocorrencia => ocorrencia.DataCriacao)
+            .ToList();
     }
 }
