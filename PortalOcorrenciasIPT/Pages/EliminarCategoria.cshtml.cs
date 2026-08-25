fc84be488716b88b012c19efaa2ba28a57e4a803
João Sousa <joaosantosesousa@gmail.com>
2026-08-25 18:44:41 +0100

    Adicionar eliminacao de categorias com Entity Framework

---
+++ PortalOcorrenciasIPT/Pages/EliminarCategoria.cshtml.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PortalOcorrenciasIPT.Data;
+using PortalOcorrenciasIPT.Models;
+
+namespace PortalOcorrenciasIPT.Pages;
+
+public class EliminarCategoriaModel : PageModel
+{
+    private readonly ApplicationDbContext _context;
+
+    public EliminarCategoriaModel(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public Categoria? Categoria { get; set; }
+
+    public IActionResult OnGet(int id)
+    {
+        Categoria = _context.Categorias
+            .FirstOrDefault(categoria => categoria.Id == id);
+
+        if (Categoria == null)
+        {
+            return RedirectToPage("/Categorias");
+        }
+
+        return Page();
+    }
+
+    public IActionResult OnPost(int id)
+    {
+        Categoria? categoria = _context.Categorias
+            .FirstOrDefault(categoria => categoria.Id == id);
+
+        if (categoria == null)
+        {
+            return RedirectToPage("/Categorias");
+        }
+
+        _context.Categorias.Remove(categoria);
+        _context.SaveChanges();
+
+        TempData["MensagemSucesso"] = "Categoria eliminada com sucesso.";
+
+        return RedirectToPage("/Categorias");
+    }
+}
