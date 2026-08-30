295984203d854b0ec32e60118f0080f7a6a71a2c
João Sousa <joaosantosesousa@gmail.com>
2026-08-30 16:26:56 +0100

    Adicionar detalhe de ocorrencia

---
+++ PortalOcorrenciasIPT/Pages/OcorrenciaDetalhe.cshtml.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PortalOcorrenciasIPT.Data;
+using PortalOcorrenciasIPT.Models;
+
+namespace PortalOcorrenciasIPT.Pages;
+
+public class OcorrenciaDetalheModel : PageModel
+{
+    private readonly ApplicationDbContext _context;
+
+    public OcorrenciaDetalheModel(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public Ocorrencia? Ocorrencia { get; set; }
+
+    public IActionResult OnGet(int id)
+    {
+        Ocorrencia = _context.Ocorrencias
+            .Include(ocorrencia => ocorrencia.Categoria)
+            .FirstOrDefault(ocorrencia => ocorrencia.Id == id);
+
+        if (Ocorrencia == null)
+        {
+            return RedirectToPage("/Ocorrencias");
+        }
+
+        return Page();
+    }
+}
