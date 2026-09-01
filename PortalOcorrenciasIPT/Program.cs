0897b809c780197512fcf7b3bd0b8b9199546541
João Sousa <joaosantosesousa@gmail.com>
2026-09-01 21:11:47 +0100

    Adicionar roles de utilizador e gestor

--- PortalOcorrenciasIPT/Program.cs
+++ PortalOcorrenciasIPT/Program.cs
@@ -11,13 +11,12 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
 builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
 {
     options.SignIn.RequireConfirmedAccount = false;
 })
+.AddRoles<IdentityRole>()
 .AddEntityFrameworkStores<ApplicationDbContext>();
-
 
 var app = builder.Build();
 
@@ -40,5 +39,22 @@ app.UseAuthorization();
 app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    string[] roles = { "Utilizador", "Gestor" };
+
+    foreach (var role in roles)
+    {
+        bool roleExiste = await roleManager.RoleExistsAsync(role);
+
+        if (!roleExiste)
+        {
+            await roleManager.CreateAsync(new IdentityRole(role));
+        }
+    }
+}
 
 app.Run();
