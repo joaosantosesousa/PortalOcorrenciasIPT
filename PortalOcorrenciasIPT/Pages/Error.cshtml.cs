fe7522dea0e8b5610eda0bd61b3ec6056848e011
João Sousa <joaosantosesousa@gmail.com>
2026-09-14 21:15:12 +0100

    Adicionar pagina de erro personalizada

--- PortalOcorrenciasIPT/Pages/Error.cshtml.cs
+++ PortalOcorrenciasIPT/Pages/Error.cshtml.cs
@@ -1,21 +1,21 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Diagnostics;
 
-namespace PortalOcorrenciasIPT.Pages
+namespace PortalOcorrenciasIPT.Pages;
+
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+public class ErrorModel : PageModel
 {
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    [IgnoreAntiforgeryToken]
-    public class ErrorModel : PageModel
+    public string? RequestId { get; set; }
+
+    public int? StatusCode { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    public void OnGet(int? statusCode = null)
     {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
-        public void OnGet()
-        {
-            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-        }
+        StatusCode = statusCode;
+        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
     }
-
 }
--- PortalOcorrenciasIPT/Program.cs
+++ PortalOcorrenciasIPT/Program.cs
@@ -44,12 +44,17 @@ builder.Services.AddAuthentication()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseStatusCodePagesWithReExecute("/Error", "?statusCode={0}");
 
 app.UseHttpsRedirection();
 
