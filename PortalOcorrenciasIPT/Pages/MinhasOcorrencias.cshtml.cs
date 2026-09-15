4b6ec17ce72b059114900fc1743a732e4436b1bc
João Sousa <joaosantosesousa@gmail.com>
2026-09-15 21:45:35 +0100

    Adicionar paginacao nas minhas ocorrencias

--- PortalOcorrenciasIPT/Pages/MinhasOcorrencias.cshtml.cs
+++ PortalOcorrenciasIPT/Pages/MinhasOcorrencias.cshtml.cs
@@ -24,20 +24,49 @@ public class MinhasOcorrenciasModel : PageModel
 
     public List<Ocorrencia> Ocorrencias { get; set; } = new();
 
-    public async Task<IActionResult> OnGetAsync()
-    {
-        ApplicationUser? utilizadorAtual = await _userManager.GetUserAsync(User);
+    public int PaginaAtual { get; set; }
 
-        if (utilizadorAtual == null)
+    public int TotalPaginas { get; set; }
+
+    public int TotalOcorrencias { get; set; }
+
+    public const int TamanhoPagina = 5;
+
+    public async Task<IActionResult> OnGetAsync(int paginaAtual = 1)
+    {
+        var utilizador = await _userManager.GetUserAsync(User);
+
+        if (utilizador == null)
         {
-            return RedirectToPage("/Index");
+            return RedirectToPage("/Identity/Account/Login");
         }
 
-        Ocorrencias = _context.Ocorrencias
-            .Include(ocorrencia => ocorrencia.Categoria)
-            .Where(ocorrencia => ocorrencia.UtilizadorId == utilizadorAtual.Id)
-            .OrderByDescending(ocorrencia => ocorrencia.DataCriacao)
-            .ToList();
+        if (paginaAtual < 1)
+        {
+            paginaAtual = 1;
+        }
+
+        PaginaAtual = paginaAtual;
+
+        var query = _context.Ocorrencias
+            .Include(o => o.Categoria)
+            .Where(o => o.UtilizadorId == utilizador.Id)
+            .OrderByDescending(o => o.DataCriacao)
+            .AsQueryable();
+
+        TotalOcorrencias = await query.CountAsync();
+
+        TotalPaginas = (int)Math.Ceiling(TotalOcorrencias / (double)TamanhoPagina);
+
+        if (TotalPaginas > 0 && PaginaAtual > TotalPaginas)
+        {
+            PaginaAtual = TotalPaginas;
+        }
+
+        Ocorrencias = await query
+            .Skip((PaginaAtual - 1) * TamanhoPagina)
+            .Take(TamanhoPagina)
+            .ToListAsync();
 
         return Page();
     }
